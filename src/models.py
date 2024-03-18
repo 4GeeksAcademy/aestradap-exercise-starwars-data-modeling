1a0bd2cedf72ee15420a06d55d9320091a0685a1
aestradap <55397157+aestradap@users.noreply.github.com>
2024-03-18 13:50:59 +0000

    diagram complete two m:m and one 1:m

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Enum, ARRAY
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Enum, ARRAY, Table
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -15,29 +15,64 @@ class User(Base):
     first_name = Column(String(20), unique=False)
     last_name = Column(String(20), unique=False)
     email = Column(String(100), unique=True)
-    user_to_id = Column(ARRAY, ForeignKey('favorite.id'))
+    favorites = relationship('Favorite', uselist=True, backref='favorites')
+    # user_to_id = Column(ARRAY, ForeignKey('favorite.id'))
     
+favorite_character_association = Table(
+    'favorite_character',
+    Base.metadata,
+    Column('favorite_id', Integer, ForeignKey('favorite.id')),
+    Column('character_id', Integer, ForeignKey('character.id'))
+)
+
+favorite_planet_association = Table(
+    'planet_character',
+    Base.metadata,
+    Column('favorite_id', Integer, ForeignKey('favorite.id')),
+    Column('planet_id', Integer, ForeignKey('planet.id'))
+)
+
 class Favorite(Base):
-    __tablename__ = 'favorites'
+    __tablename__ = 'favorite'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    planet_id = Column(Integer, ForeignKey('planet.id'))
-    character_id = Column(Integer, ForeignKey('character.id'))
+    planets = relationship('Planet', secondary=favorite_planet_association, back_populates='favorites')
+    characters = relationship('Character', secondary=favorite_character_association, back_populates='favorites')
+    
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(20), unique=False)
+    favorites = relationship('Favorite', secondary=favorite_character_association, back_populates='characters')
 
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
     planet_name = Column(String(20), unique=False)
-    favorite_id = Column(Integer, ForeignKey('favorite.id'))
+    favorites = relationship('Favorite', secondary=favorite_planet_association, back_populates='planets')
 
-class character(Base):
-    __tablename__ = 'character'
-    id = Column(Integer, primary_key=True)
-    character_name = Column(String(20), unique=False)
-    favorite_id = Column(Integer, ForeignKey('favorite.id'))
+
+
+   
     
+#     article_author_association = Table(
+#     'article_author',
+#     Base.metadata,
+#     Column('article_id', Integer, ForeignKey('articles.id')),
+#     Column('author_id', Integer, ForeignKey('users.id'))
+# )
 
+# class User(Base):
+#     __tablename__ = 'users'
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String)
+#     articles = relationship('Article', secondary=article_author_association, back_populates='authors')
 
+# class Article(Base):
+#     __tablename__ = 'articles'
+#     id = Column(Integer, primary_key=True)
+#     title = Column(String)
+#     authors = relationship('User', secondary=article_author_association, back_populates='articles')
  
     def to_dict(self):
         return {}
